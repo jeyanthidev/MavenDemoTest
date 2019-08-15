ea6c8563e0578ef5c205ffc292f7005dbe3f4a7f
jeyanthidevush <jeyanthidev@gmail.com>
2019-08-14 22:05:46 -0500

    test

---
+++ src/test/java/com/qa/test/DemoTest1.java
@@ -0,0 +1,37 @@
+package com.qa.test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DemoTest1 {
+
+	 @Test
+		public void sum(){
+		  System.out.println("sum");
+		  int a=10;
+		  int b=20;
+		  Assert.assertEquals(30,a+b);
+	  }
+	  @Test
+	  public void sub(){
+		  System.out.println("sub");
+		  int a=20;
+		  int b=10;
+		  Assert.assertEquals(10,a-b);
+	  }
+	  @Test
+		public void multi(){
+		  System.out.println("multi");
+		  int a=10;
+		  int b=20;
+		  Assert.assertEquals(200,a*b);
+	}  
+	  @Test
+		public void div(){
+		  System.out.println("div");
+		  int a=10;
+		  int b=20;
+		  Assert.assertEquals(2,b/a);
+	}
+
+}
